bdbf8fe8a5c45d176f8d3d57c3eafeecbcdbb5c7
Eleonore de Bokay <mmscdebokay@icloud.com>
2022-06-03 14:35:59 +0200

    changes in main.py

--- tovoice/main.py
+++ tovoice/main.py
@@ -19,6 +19,8 @@ def pad_seq(x, base=32):
 
 device = "cpu"
 G = Generator().eval().to(device)
+mod = torch.load('generator.ckpt')
+G.load_state_dict(mod)
 
 
 #a = pickle.load(open("/Users/eleonoredebokay/Downloads/metadata.pkl", 'rb'))
@@ -44,7 +46,55 @@ with torch.no_grad():  # doesn't charge the model memory
 
 print(psnt.shape)
 
+spect_vc = []
+with torch.no_grad():
+    _, psnt, _ = G(spec, emb1, emb2)
+    if len_pad == 0:
+        trgt_uttr = psnt[0, 0, :, :].cpu().numpy()
+    else:
+        trgt_uttr = psnt[0, 0, :-len_pad, :].cpu().numpy()
+
+    #spect_vc.append( ('{}x{}'.format(emb1,emb2), trgt_uttr) )
+    spect_vc.append( ('contentXspeaker', trgt_uttr) )
+
+
+with open('results.pkl', 'wb') as handle:
+    pickle.dump(spect_vc, handle)
+
 
 exit()
 
 G(spec, emb1, emb2)
+
+metadata = pickle.load(open('metadata.pkl', "rb"))
+
+
+#for sbmt_i in metadata:
+#
+#    x_org = sbmt_i[2]
+#    x_org, len_pad = pad_seq(x_org)
+#    uttr_org = torch.from_numpy(x_org[np.newaxis, :, :]).to(device)
+#    emb_org = torch.from_numpy(sbmt_i[1][np.newaxis, :]).to(device)
+#
+#    for sbmt_j in metadata:
+#
+#        emb_trg = torch.from_numpy(sbmt_j[1][np.newaxis, :]).to(device)
+#
+        with torch.no_grad():
+            _, psnt, _ = G(uttr_org, emb_org, emb_trg)
+
+        if len_pad == 0:
+            trgt_uttr = psnt[0, 0, :, :].cpu().numpy()
+        else:
+            trgt_uttr = psnt[0, 0, :-len_pad, :].cpu().numpy()
+
+        spect_vc.append( ('{}x{}'.format(sbmt_i[0], sbmt_j[0]), trgt_uttr) )
+
+
+with open('results.pkl', 'wb') as handle:
+    pickle.dump(spect_vc, handle)
+
+
+
+# show spec and emb
+# show concat of spec and emb
