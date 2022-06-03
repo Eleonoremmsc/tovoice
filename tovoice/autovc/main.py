1ee1ca82d3aa53a665ebeb6e1c78c2a939dda8e1
Eleonore de Bokay <mmscdebokay@icloud.com>
2022-06-03 17:30:56 +0200

    add auto_main

--- tovoice/autovc/main.py
+++ tovoice/autovc/main.py
@@ -2,13 +2,8 @@ from copyreg import pickle
 import torch
 import numpy as np
 from math import ceil
-
+import os
 from autovc import Generator # -> can I remove ?
-
-device = "cpu"
-G = Generator().eval().to(device)
-mod = torch.load('generator.ckpt')
-G.load_state_dict(mod)
 
 def pad_seq(x, base=32):
     len_out = int(base * ceil(float(x.shape[0])/base))
@@ -17,7 +12,17 @@ def pad_seq(x, base=32):
     return np.pad(x, ((0,len_pad),(0,0)), 'constant'), len_pad
 
 
-def auto_main(spec, emb1, emb2):
+def auto_main(spec_file, emb1_name, emb2_name):
+
+    spec = np.load(os.path.join("/data/spectrograms", spec_file))
+    emb1 = torch.load(os.path.join("/data/speaker-embeddings", emb1_name))
+    emb2 = torch.load(os.path.join("/data/speaker-embeddings", emb1_name))
+
+    device = "cpu"
+    G = Generator().eval().to(device)
+    mod = torch.load('generator.ckpt')
+    G.load_state_dict(mod)
+
     # Preprocess spec
     spec, len_pad = pad_seq(spec)
     spec = torch.from_numpy(spec[np.newaxis, :, :])
@@ -26,10 +31,10 @@ def auto_main(spec, emb1, emb2):
     with torch.no_grad():
         _, psnt, _ = G(spec, emb1, emb2)
         trgt_uttr = psnt[0, 0, :-len_pad, :].cpu().numpy()
-        spect_vc.append( ('contentXspeaker', trgt_uttr) )
+        spect_vc.append( ('{}x{}'.format(spec,G), trgt_uttr) )
 
 
-    with open('results.pkl', 'wb') as handle:
+    with open('{}x{}.pkl'.format(emb1_name,emb2_name), 'wb') as handle:
         pickle.dump(spect_vc, handle)
 
 
---
+++ tovoice/main.py
@@ -0,0 +1,6 @@
+from autovc import pad_seq, auto_main
+
+spec_file =
+emb1_name, emb2_name =
+
+auto_main(spec_file, emb1_name, emb2_name)
